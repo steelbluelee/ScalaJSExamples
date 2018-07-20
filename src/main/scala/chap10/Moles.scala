754e3698c99e01b8ae9f9175dbc399073c2a7134
SeungKyu <steelblue.lee@gmail.com>
2018-07-20 16:58:24 +0900

    Moles game 2

--- src/main/scala/chap10/Moles.scala
+++ src/main/scala/chap10/Moles.scala
@@ -13,55 +13,62 @@ import cats._
 import cats.implicits._
 import cats.data.Reader
 
-case class Mole(x: Int, y: Int, opacity: Double)
-
 @JSExportTopLevel("Moles")
 object Moles {
+  type Mole = dom.html.Div
+  private var moles: Set[dom.html.Div] = Set()
+  val W                                = 50
+  val SPACE                            = 10
+
   @JSExport
   def main(): Unit = {
-    makeMoles(7, 4)
+    makeAMoles(7, 4)
   }
 
-  def makeMoles(nx: Int, ny: Int) = {
-    var molesState: Map[dom.html.Div, Mole] = Map()
-    val W                                   = 50
-    val SPACE                               = 10
+  private def makeAMoles(nx: Int, ny: Int) = {
     for (i <- 0 to nx - 1;
          j <- 0 to ny - 1) {
-      val element = div.render
-
-      // this code is same as the previouse code
-      // val element = div.render
-      //
-      // val element = js.Dynamic.global.document
-      //   .createElement("div")
-      //   .asInstanceOf[dom.html.Div]
-
-      element.style.width      = W + "px"
-      element.style.height     = W + "px"
-      element.style.background = "url(../images/mole.jpg)"
-      element.style.position   = "absolute"
-      element.style.opacity    = 0.2.toString
-      element.style.transition = "transform 0.5s ease-in-out, opacity 0.5s ease"
-
+      val element = makeAMole(i, j)
       js.Dynamic.global.document.body.appendChild(element)
-
-      element.style.left = SPACE + i * (W + SPACE) + "px"
-      element.style.top  = 2 * SPACE + j * (W + SPACE) + "px"
-
-      molesState = molesState + (element -> Mole(i, j, 0.2))
-
-      element.onclick = (e: MouseEvent) => {
-        val element = e.currentTarget.asInstanceOf[dom.html.Div]
-        (molesState get element) foreach { state =>
-          if (state.opacity >= 0.5) {
-            js.Dynamic.global.document.body.removeChild(element)
-            molesState -= element
-          }
-        }
-      }
+      moles += element
+      registerEventHandler(element)
     }
-
-    molesState
+    moles
   }
+
+  private def makeAMole(i: Int, j: Int): Mole = {
+
+    // The following one line is same as
+    //
+    // val element = js.Dynamic.global.document
+    //   .createElement("div")
+    //   .asInstanceOf[dom.html.Div]
+    val element = div.render
+
+    element.style.width      = W + "px"
+    element.style.height     = W + "px"
+    element.style.background = "url(../images/mole.jpg)"
+    element.style.position   = "absolute"
+    element.style.opacity    = 1.0.toString
+    element.style.transition = "transform 0.5s ease-in-out, opacity 0.5s ease"
+
+    element.style.left = SPACE + i * (W + SPACE) + "px"
+    element.style.top  = 2 * SPACE + j * (W + SPACE) + "px"
+
+    js.Dynamic.global.console.log(element.style.left)
+    js.Dynamic.global.console.log(element.style.top.toString)
+
+    element
+  }
+
+  private def registerEventHandler(element: dom.html.Div): Unit =
+    element.onclick = (e: MouseEvent) => {
+      val element = e.currentTarget.asInstanceOf[dom.html.Div]
+      js.Dynamic.global.console.log(moles.size)
+      if (element.style.opacity.toDouble > 0.5) {
+        js.Dynamic.global.document.body.removeChild(element)
+        moles -= element
+      }
+      js.Dynamic.global.console.log(moles.size)
+    }
 }
