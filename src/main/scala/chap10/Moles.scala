1ec717a90a79448e1836502fbb02bf45a619e512
SeungKyu <steelblue.lee@gmail.com>
2018-07-20 19:16:13 +0900

    Finished mole game

--- src/main/scala/chap10/Moles.scala
+++ src/main/scala/chap10/Moles.scala
@@ -1,6 +1,7 @@
 package ScalaJSExamples
 
 import scala.util.{Try, Success, Failure}
+import scala.util.Random
 
 import scala.scalajs.js.annotation.{JSExport, JSExportTopLevel}
 import org.scalajs.dom
@@ -15,14 +16,19 @@ import cats.data.Reader
 
 @JSExportTopLevel("Moles")
 object Moles {
-  type Mole = dom.html.Div
-  private var moles: Set[dom.html.Div] = Set()
-  val W                                = 50
-  val SPACE                            = 10
+  type Mole  = dom.html.Div
+  type Moles = Set[dom.html.Div]
+  private var _moles: Set[dom.html.Div]              = Set()
+  var timer: js.UndefOr[js.timers.SetIntervalHandle] = js.undefined
+  val W                                              = 50
+  val SPACE                                          = 10
+  val TIME_INTERVAL                                  = 1500
+  val DISPLAY_TIME                                   = 1050
 
   @JSExport
   def main(): Unit = {
     makeAMoles(7, 4)
+    timer = js.timers.setInterval(TIME_INTERVAL)(game run _moles)
   }
 
   private def makeAMoles(nx: Int, ny: Int) = {
@@ -30,10 +36,10 @@ object Moles {
          j <- 0 to ny - 1) {
       val element = makeAMole(i, j)
       js.Dynamic.global.document.body.appendChild(element)
-      moles += element
+      _moles += element
       registerEventHandler(element)
     }
-    moles
+    _moles
   }
 
   private def makeAMole(i: Int, j: Int): Mole = {
@@ -49,14 +55,14 @@ object Moles {
     element.style.height     = W + "px"
     element.style.background = "url(../images/mole.jpg)"
     element.style.position   = "absolute"
-    element.style.opacity    = 1.0.toString
+    element.style.opacity    = 0.2.toString
     element.style.transition = "transform 0.5s ease-in-out, opacity 0.5s ease"
 
     element.style.left = SPACE + i * (W + SPACE) + "px"
     element.style.top  = 2 * SPACE + j * (W + SPACE) + "px"
 
-    js.Dynamic.global.console.log(element.style.left)
-    js.Dynamic.global.console.log(element.style.top.toString)
+    // js.Dynamic.global.console.log(element.style.left)
+    // js.Dynamic.global.console.log(element.style.top.toString)
 
     element
   }
@@ -64,11 +70,36 @@ object Moles {
   private def registerEventHandler(element: dom.html.Div): Unit =
     element.onclick = (e: MouseEvent) => {
       val element = e.currentTarget.asInstanceOf[dom.html.Div]
-      js.Dynamic.global.console.log(moles.size)
+      // js.Dynamic.global.console.log(_moles.size)
       if (element.style.opacity.toDouble > 0.5) {
         js.Dynamic.global.document.body.removeChild(element)
-        moles -= element
+        _moles -= element
       }
-      js.Dynamic.global.console.log(moles.size)
+      // js.Dynamic.global.console.log(_moles.size)
     }
+
+  private val game: Reader[Moles, Unit] = for {
+    molesNum <- Reader((moles: Moles) => moles.size)
+    _ <- if (molesNum == 0) // format: off
+           Reader((_: Moles) => stopGame())
+         else
+           Reader((_: Moles) => continueGame(_moles))
+         // format: on
+  } yield ()
+
+  private def stopGame(): Unit = {
+    timer foreach js.timers.clearInterval
+    js.Dynamic.global.document.body.innerHTML = "game over"
+  }
+
+  private def continueGame(moles: Moles) = {
+    val r    = Random
+    val mole = moles.toList(r nextInt (moles.size))
+    mole.style.opacity   = 1.toString
+    mole.style.transform = "translateY(-10px)"
+    js.timers.setTimeout(DISPLAY_TIME) {
+      mole.style.opacity   = 0.2.toString
+      mole.style.transform = "translateY(0px)"
+    }
+  }
 }
