4fbda5e7328d43444f60e68e3044e5b7b52e52a0
SeungKyu <steelblue.lee@gmail.com>
2018-08-28 15:43:39 +0900

    2018/08/28 02

--- src/main/scala/chap16/XMLHttpRequestExample.scala
+++ src/main/scala/chap16/XMLHttpRequestExample.scala
@@ -26,4 +26,84 @@ object XMLHttpRequestExample {
     req.open("GET", "data.txt")
     req.send()
   }
+
+  @JSExport
+  def readyStateChange(): Unit = {
+    val req = new XMLHttpRequest()
+
+    g.console.log(s"A: readyState = ${req.readyState}")
+
+    req.onreadystatechange = (_: Event) =>
+      g.console.log(s"B: readyState = ${req.readyState}")
+
+    req.open("GET", "data.txt")
+    req.send()
+  }
+
+  @JSExport
+  def load(): Unit = {
+    val req = new XMLHttpRequest()
+
+    req.addEventListener("load",
+                         (_: Event) =>
+                           g.document.getElementById("view").innerHTML =
+                             req.responseText,
+                         false)
+    req.open("GET", "data.txt")
+    req.send()
+  }
+
+  @JSExport
+  def loadstart(): Unit = {
+    val req = new XMLHttpRequest()
+
+    req.addEventListener("loadstart",
+                         (_: Event) =>
+                           g.document.getElementById("view").innerHTML =
+                           "on processing",
+                         false)
+    req.open("GET", "data.txt")
+    req.send()
+  }
+
+  @JSExport
+  def error(): Unit = {
+    val req = new XMLHttpRequest()
+
+    req.addEventListener("error",
+                         (_: Event) =>
+                           g.document.getElementById("view").innerHTML =
+                           "Server Error",
+                         false)
+    req.open("GET", "data.txt")
+    req.send()
+  }
+
+  //see https://github.com/scala-js/scala-js/blob/master/library/src/main/scala/scala/scalajs/js/JSON.scala
+  @JSExport
+  def responseText(): Unit = {
+    val req = new XMLHttpRequest()
+
+    req.addEventListener("load", (_: Event) => {
+      val jsonObj = js.JSON.parse(req.responseText)
+      g.console.log(jsonObj)
+    }, false)
+
+    req.open("GET", "data.json", true)
+    req.send()
+  }
+
+  @JSExport
+  def response(): Unit = {
+    val req = new XMLHttpRequest()
+
+    req.addEventListener("load", (_: Event) => {
+      val jsonObj = req.response
+      g.console.log(jsonObj)
+    }, false)
+
+    req.responseType = "json"
+    req.open("GET", "data.json", true)
+    req.send()
+  }
 }
